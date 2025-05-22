1715d38debf0e0879c3891aa4e25d81b4cdb9962
edcaetanoguedes <ednaldocaetanoguedes@gmail.com>
2025-05-21 21:25:26 -0300

    chore: instalação e configuração das ferramentas de revisão e padronização de código e commit

---
+++ prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  semi: true, // ; no final das linhas
+  singleQuote: true, // aspas simples ao invés de dupla
+  trailingComma: "all", // ; no final de objetos ex: const `obj = {}`;
+  tabWidth: 2, // número de espaços por identação
+  printWidth: 100, // a partir 100 caracteres a linha quebra
+  bracketSpacing: true, // espaço entre chave e valor de objetos
+};
