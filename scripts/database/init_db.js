83d3c884fa992975da755bf9bf9311a49fd7eecd
edcaetanoguedes <ednaldocaetanoguedes@gmail.com>
2025-05-25 23:17:59 -0300

    feat(database): implementação do método de reset do banco de dados

--- scripts/database/init_db.js
+++ scripts/database/init_db.js
@@ -3,7 +3,16 @@ const path = require("path");
 const fs = require("fs");
 
 const dbDir = path.join(process.cwd(), "database"); // Diretório do banco de dados local
-const dbPath = path.join(__dirname, "../database/database.db");
+const dbPath = path.join(dbDir, "./database.db");
+
+const TABLES = {
+  tasks: "tasks",
+  tasks_status: "tasks_status",
+};
+
+// Opções iniciais de status para tarefas
+// "pendente" DEVE ser o primeiro registro da tabela, pois seu ID será utilizado como padrão no cadastro de novas tarefas
+const tasks_status_initial = [{ title: "pendente" }, { title: "concluído" }];
 
 if (!fs.existsSync(dbDir)) {
   fs.mkdirSync(dbDir, { recursive: true });
@@ -22,11 +31,49 @@ const db = new sqlite3.Database(dbPath, (err) => {
 
 db.serialize(() => {
   db.run(`
-        CREATE TABLE IF NOT EXISTS tasks (
+        CREATE TABLE IF NOT EXISTS ${TABLES.tasks} (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             text TEXT NOT NULL,
+            status TEXT NOT NULL DEFAULT "${tasks_status_initial[0].title}",
             creation TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )`);
+
+  db.run(`
+        CREATE TABLE IF NOT EXISTS ${TABLES.tasks_status} (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,    
+          title TEXT UNIQUE NOT NULL
+        )`);
+
+  // Inicialização das tabelas
+
+  // Tabela Opções de Status de tarefas
+  tasks_status_initial.forEach((status) => {
+    db.run(
+      `INSERT INTO ${TABLES.tasks_status} (title) VALUES (?)`,
+      [status.title],
+      (err) => {
+        if (err) {
+          return console.log(
+            `Erro ao adicionar a opção de status "${status.title}"!`,
+          );
+        }
+
+        console.log(
+          `Opção de status(${status.title}) de task adicionada com sucesso!`,
+        );
+      },
+    );
+  });
+
+  console.log("Opções de status adicionadas!");
+
+  // db.close((closeErr) => {
+  //     if(closeErr) {
+  //       console.error("Erro ao fechar o banco de dados!")
+  //     } else {
+  //       console.error("Conexão com o banco de dados fechada com sucesso!")
+  //     }
+  //   })
 });
 
-module.exports = db;
+module.exports = { db, TABLES };
