18a26f5c9e6deb0e5d029ae536afc9a4d9e59844
edcaetanoguedes <ednaldocaetanoguedes@gmail.com>
2025-05-25 23:27:58 -0300

    refactor(server): aprimoramento de código

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const cors = require("cors"); // controla quais origens externas podem fazer requisições
 const bodyParser = require("body-parser");
-const db = require("./scripts/database/init_db");
-const Sqlite = require("./scripts/database/procediments");
+const Sqlite = require("./scripts/database/procedures");
 
 const app = express();
 const port = 4000;
@@ -10,7 +9,38 @@ const port = 4000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Consultar todas as tarefas cadastradas
+// Consultar todas as opções de tarefas
+app.get(`/api/v1/tasks/status`, (req, res) => {
+  Sqlite.tasks_selectAllStatus((err, rows) => {
+    if (err) {
+      console.log("Error: ", err.message);
+      return res.status(500).json({ error: err.message });
+    }
+
+    res.status(200).json(rows);
+  });
+});
+
+// Consultar uma opção de status pelo ID
+app.get(`/api/v1/tasks/status/:id`, (req, res) => {
+  const { id } = req.params;
+
+  Sqlite.tasks_selectStatusById(id, (err, rows) => {
+    if (err) {
+      console.log("Error: ", err.message);
+      return res.status(500).json({ error: err.message });
+    }
+
+    if (!rows) {
+      console.log("Error: ", "Opção de status não encontrada!");
+      return res.status(400).json({ error: "Opção de status não encontrada!" });
+    }
+
+    res.status(200).json(rows);
+  });
+});
+
+// Consultar todas as tarefas
 app.get(`/api/v1/tasks`, (req, res) => {
   Sqlite.tasks_selectAll((err, rows) => {
     if (err) {
@@ -31,21 +61,27 @@ app.get(`/api/v1/tasks/:id`, (req, res) => {
       console.log("Error: ", err.message);
       return res.status(500).json({ error: err.message });
     }
+
+    if (!rows) {
+      console.log("Error: ", "Tarefa não encontrada!");
+      return res.status(400).json({ error: "Tarefa não encontrada!" });
+    }
+
     res.status(200).json(rows);
   });
 });
-
 // Cadastrar nova tarefa
 app.post(`/api/v1/tasks`, (req, res) => {
   const { text } = req.body;
 
-  Sqlite.tasks_submitNewTask({ text }, (err) => {
+  Sqlite.tasks_submitNewTask({ text }, (err, rows) => {
     if (err) {
       console.log("Error: ", err.message);
       return res.status(500).json({ error: err.message });
     }
-    res.status(201).json({ id: this.lastID });
   });
+
+  res.status(201).json({ message: "Tarefa criada com sucesso!" });
 });
 
 // Deletar tarefa pelo ID
@@ -53,19 +89,56 @@ app.delete(`/api/v1/tasks/:id`, (req, res) => {
   console.log("Request DELETE /api/v1/tasks/:id");
   const { id } = req.params;
 
-  Sqlite.tasks_deleteTaskById(id, (err) => {
+  console.log("params.id: ", id);
+
+  Sqlite.tasks_selectById(id, (err, rows) => {
     if (err) {
       console.log("Error: ", err.message);
-      return res.status(500).send({ error: "Erro ao remover tarefa!" });
+      return res.status(500).json({ error: err.message });
     }
 
-    // Caso a tarefa não seja encontrada
-    if (this.changes == 0) {
-      console.log("Error: ", "Tarefa não encontrada!");
-      return res.status(500).send({ error: "Tarefa não encontrada!" });
+    // Verifica a existência da tarefa
+    if (!rows) {
+      return res.status(400).json({ message: "Esta tarefa não existe!" });
     }
 
-    res.status(200).json({ message: "Tarefa removida com sucesso!" });
+    // Deleta a tarefa
+    Sqlite.tasks_deleteById(id, (err) => {
+      if (err) {
+        console.log("Error: ", err.message);
+        return res.status(500).json({ error: err.message });
+      }
+
+      res.status(200).json({ message: `Tarefa ${id} apagada com sucesso!` });
+    });
+  });
+});
+
+// Atualizar informações da tarefa
+app.put(`/api/v1/tasks/:id`, (req, res) => {
+  const { id } = req.params;
+  const { text, status } = req.body;
+
+  Sqlite.tasks_selectById(id, (err, rows) => {
+    if (err) {
+      console.log("Error: ", err.message);
+      return res.status(500).json({ error: err.message });
+    }
+
+    // Verifica a existência da tarefa
+    if (!rows) {
+      return res.status(400).json({ message: "Esta tarefa não existe!" });
+    }
+
+    // Atualiza os dados da tarefa
+    Sqlite.tasks_updateById(id, { text, status }, (err) => {
+      if (err) {
+        console.log("Erro ao atualizar a task!");
+        return res.status(500).json({ error: err.message });
+      }
+
+      res.status(200).json({ message: "Tarefa atualizada com sucesso!" });
+    });
   });
 });
 
